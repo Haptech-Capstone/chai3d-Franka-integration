1fbbfa179aa19e729fa1dc8fea1902c93db803d6
pehilbert <pehilbert03@gmail.com>
2025-04-18 13:22:53 -0700

    Start server prototype

---
+++ examples/GLFW/haptech-prototype/main.cpp
@@ -0,0 +1,9 @@
+#include "prototype_sim.cpp"
+
+int main(int argc, char* argv[]) {
+    std::cout << "Press Enter to start the sim:";
+    std::cin.get();
+    
+    load(argc, argv);
+    return 0;
+}
